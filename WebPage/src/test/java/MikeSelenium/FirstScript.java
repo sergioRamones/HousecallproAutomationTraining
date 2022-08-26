2b76e785f9427fe9a250d5ed1b8cf67e52ca8213
mikeu825 <63549198+mikeu825@users.noreply.github.com>
2022-08-26 14:12:42 -0500

    Updates to Mike's first script
    - Added edge driver method
    - Utilized explicit wait command

--- WebPage/src/test/java/MikeSelenium/FirstScript.java
+++ WebPage/src/test/java/MikeSelenium/FirstScript.java
@@ -1,28 +1,92 @@
 package MikeSelenium;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class FirstScript {
 	
-	@Test
+	WebDriver driver;
+	WebDriverWait wait;
 	
-	public void firstScript() throws InterruptedException {
-		///Users/michael.untalan/Desktop/git/automationTraining/HousecallproAutomationTraining/WebPage/chromedriver/chromedriver
-		String pathChromeDriver = System.getProperty("user.dir");
-		System.setProperty("webdriver.chrome.driver", pathChromeDriver + "/chromedriver/chromedriver");
-		
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-		Thread.sleep(3000);
-		driver.manage().window().maximize();
-		driver.findElement(By.xpath("//*[@name='username']")).sendKeys("Admin");
-		//driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
-		driver.findElement(By.name("password")).sendKeys("admin123");
-		
-		driver.findElement(By.xpath("//button[contains(@class,'login')]")).click();
-	}
+	
+//	@Test
+//	public void fistrScript() throws InterruptedException {
+//
+//		startBrowser("https://opensource-demo.orangehrmlive.com/","chrome");		
+//		wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+//		
+//		
+//		driver.findElement(By.xpath("//*[@name='username']")).sendKeys("Admin");
+//		driver.findElement(By.name("password")).sendKeys("admin123");
+//		driver.findElement(By.xpath("//button[contains(@class,'login')]")).click();
+//		
+//		
+//		
+//	}
+	
+	@Test
+	public void secondScript() throws InterruptedException {
 
+		startBrowser("https://opensource-demo.orangehrmlive.com/","edge");		
+		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@name='username']"))).sendKeys("Admin");
+		//driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[contains(@class,'login')]")).click();
+		
+		
+		
+	}
+	
+	public void startBrowser(String url,  String  browser){
+		
+		switch(browser) {
+		
+		case "chrome":
+			String pathChromeDriver= System.getProperty("user.dir");
+			System.setProperty("webdriver.chrome.driver", pathChromeDriver +"/chromedriver/chromedriver");
+			
+			ChromeOptions option = new ChromeOptions();
+			option.addArguments("--start-maximized");
+			option.addArguments("--incognito");
+			driver = new ChromeDriver(option);
+			driver.get(url);
+			
+			break;
+			
+		case "edge":
+			//EdgeDrvier
+			
+			String pathEdgeDriver= System.getProperty("user.dir");
+			System.setProperty("webdriver.edge.driver", pathEdgeDriver +"/edgeDriver/msedgedriver");
+			
+			EdgeOptions edgeOption = new EdgeOptions();
+			edgeOption.addArguments("--start-maximized");
+			edgeOption.addArguments("--incognito");
+			driver = new EdgeDriver(edgeOption);
+			driver.get(url);
+			
+			break;
+			
+		case "firefox":
+			
+			//FirefoxDriver
+			break;
+			
+		default: System.out.println("Option is not allowed");
+		
+		}
+		
+		
+	}
 }
