cbf0c972f7bdca86a43a2252db67a3eaf898be34
Sergio Ramones <sergio.ramones@housecallpro.com>
2022-08-26 15:30:42 -0500

    update script

---
+++ WebPage/edgedriver/readme.txt
@@ -0,0 +1,1 @@
+put edge driver here
--- WebPage/src/test/java/sergioSelenium/FirstScript.java
+++ WebPage/src/test/java/sergioSelenium/FirstScript.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class FirstScript {
@@ -23,13 +24,31 @@ public class FirstScript {
 		startBrowser("https://opensource-demo.orangehrmlive.com/","chrome");		
 		wait = new WebDriverWait(driver, Duration.ofSeconds(60));
 		
+		wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("h5"), "Login"));
+		typeText(By.xpath("//*[@name='username']"),"Admin");
+		typeText(By.name("password"),"admin123");
+	
 		
-		driver.findElement(By.xpath("//*[@name='username']")).sendKeys("Admin");
-		driver.findElement(By.name("password")).sendKeys("admin123");
+	//	driver.findElement(By.xpath("//*[@name='username']")).sendKeys("Admin");
+//		driver.findElement(By.name("password")).sendKeys("admin123");
+	
 		driver.findElement(By.xpath("//button[contains(@class,'login')]")).click();
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.className("oxd-userdropdown-name"))));
+		
+		click(By.xpath("//label[text()='Sub Unit']/parent::*/parent::*//i[@class='oxd-icon bi-caret-down-fill oxd-select-text--arrow']"));
+		click(By.xpath("//span[contains(text(),'Development')]"));
 		
 		
-		
+	}
+	
+	public void typeText(By locator, String word) {
+		driver.findElement(locator).clear();
+		driver.findElement(locator).sendKeys(word);
+		Reporter.log("The word was typed [ "+word+" ]",true);
+	}
+	
+	public void click(By locator) {
+		driver.findElement(locator).click();
 	}
 	
 	
