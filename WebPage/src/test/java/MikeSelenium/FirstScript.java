8bd1eae0718c43eb46d6ca3ee25e3edb83186976
mikeu825 <63549198+mikeu825@users.noreply.github.com>
2022-09-02 12:25:04 -0500

    First Script Update
    I updated my script to fill out the remaining employee fields and complete the automated test.

--- WebPage/src/test/java/MikeSelenium/FirstScript.java
+++ WebPage/src/test/java/MikeSelenium/FirstScript.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class FirstScript {
@@ -18,35 +19,61 @@ public class FirstScript {
 	WebDriverWait wait;
 	
 	
-//	@Test
-//	public void fistrScript() throws InterruptedException {
-//
-//		startBrowser("https://opensource-demo.orangehrmlive.com/","chrome");		
-//		wait = new WebDriverWait(driver, Duration.ofSeconds(60));
-//		
-//		
-//		driver.findElement(By.xpath("//*[@name='username']")).sendKeys("Admin");
-//		driver.findElement(By.name("password")).sendKeys("admin123");
-//		driver.findElement(By.xpath("//button[contains(@class,'login')]")).click();
-//		
-//		
-//		
-//	}
-	
 	@Test
-	public void secondScript() throws InterruptedException {
+	public void fistrScript() throws InterruptedException {
 
-		startBrowser("https://opensource-demo.orangehrmlive.com/","edge");		
-		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		startBrowser("https://opensource-demo.orangehrmlive.com/","chrome");		
+		wait = new WebDriverWait(driver, Duration.ofSeconds(60));
 		
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@name='username']"))).sendKeys("Admin");
-		//driver.findElement(By.name("username")).sendKeys("Admin");
-		driver.findElement(By.name("password")).sendKeys("admin123");
+		wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("h5"), "Login"));
+		typeText(By.xpath("//*[@name='username']"),"Admin");
+		typeText(By.name("password"),"admin123");
+
+		
+	//	driver.findElement(By.xpath("//*[@name='username']")).sendKeys("Admin");
+//		driver.findElement(By.name("password")).sendKeys("admin123");
+	
 		driver.findElement(By.xpath("//button[contains(@class,'login')]")).click();
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.className("oxd-userdropdown-name"))));
 		
+		selectFromDropDown("Sub Unit","Development");
+		selectFromDropDown("Sub Unit","Administration");
+		
+		selectFromDropDown("Employment Status","Full-Time Contract");
+		selectFromDropDown("Job Title","Chief Executive Officer");
+		
+		//new method for testing name fields
+		typeName("Employee Name", "Type for hints...", "Mike");
+		typeName("Supervisor Name", "Type for hints...", "Rachana");
+		typeText(By.xpath("//label[text()='Employee Id']/following::*//input[@class='oxd-input oxd-input--active']"), "qa@Hcp1234");
+		
+		//Finish
+		click(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space']"));
+	}
+	
+	
+	public void selectFromDropDown(String SubUnit, String Option) throws InterruptedException {
+		click(By.xpath("//label[text()='"+SubUnit+"']/parent::*/parent::*//i[@class='oxd-icon bi-caret-down-fill oxd-select-text--arrow']"));
+		click(By.xpath("//span[contains(text(),'"+Option+"')]"));
+		Thread.sleep(1000);
+	}
+	
+	public void typeName(String labelText, String placeHolder, String text) throws InterruptedException {
+		driver.findElement(By.xpath("//label[text()='"+labelText+"']/following::*/descendant::*//input[@placeholder='"+placeHolder+"']")).sendKeys(text);
 		
 		
 	}
+	
+	public void typeText(By locator, String word) {
+		driver.findElement(locator).clear();
+		driver.findElement(locator).sendKeys(word);
+		Reporter.log("The word was typed [ "+word+" ]",true);
+	}
+	
+	public void click(By locator) {
+		driver.findElement(locator).click();
+	}
+	
 	
 	public void startBrowser(String url,  String  browser){
 		
@@ -65,17 +92,8 @@ public class FirstScript {
 			break;
 			
 		case "edge":
+			
 			//EdgeDrvier
-			
-			String pathEdgeDriver= System.getProperty("user.dir");
-			System.setProperty("webdriver.edge.driver", pathEdgeDriver +"/edgeDriver/msedgedriver");
-			
-			EdgeOptions edgeOption = new EdgeOptions();
-			edgeOption.addArguments("--start-maximized");
-			edgeOption.addArguments("--incognito");
-			driver = new EdgeDriver(edgeOption);
-			driver.get(url);
-			
 			break;
 			
 		case "firefox":
