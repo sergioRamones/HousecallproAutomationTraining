10b795196cf2dc3352d392df67830d5909f5f4e2
Sergio Ramones <sergio.ramones@housecallpro.com>
2022-09-07 12:59:59 -0500

    init drivers

--- WebPage/src/main/java/utility/CommonMethods.java
+++ WebPage/src/main/java/utility/CommonMethods.java
@@ -1,6 +1,14 @@
 package utility;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class CommonMethods {
 	
@@ -10,6 +18,11 @@ public class CommonMethods {
 	private String chromeDriver;
 	private String edgeDriver;
 	private String firefoxDriver;
+	
+	
+	public CommonMethods() {
+		setDriverPaths();
+	}
 	
 	public String getOSName() {
 		if(osName.contains("Mac")) {
@@ -37,6 +50,36 @@ public class CommonMethods {
 			edgeDriver = projectPath +"\\edgedriver\\msedgedriver.exe";
 			firefoxDriver = projectPath +"\\geckodriver\\geckodriver.exe";
 		}
+	}
+	
+	public WebDriver chromeDriverConnection() {
+		System.setProperty("webdriver.chrome.driver", chromeDriver);
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--start-maximized");
+		option.addArguments("--incognito");
+		driver = new ChromeDriver(option);
+		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(200));
+		return driver;
+	}
+	
+	public WebDriver edgeDriverConnection() {
+		System.setProperty("webdriver.edge.driver", edgeDriver);
+		EdgeOptions option = new EdgeOptions();
+		option.addArguments("--start-maximized");
+		option.addArguments("--incognito");
+		driver = new EdgeDriver(option);
+		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(200));
+		return driver;
+	}
+	
+	public WebDriver firefoxDriverConnection() {
+		System.setProperty("webdriver.chrome.driver", chromeDriver);
+		FirefoxOptions option = new FirefoxOptions();
+		option.addArguments("--start-maximized");
+		option.addArguments("--incognito");
+		driver = new FirefoxDriver(option);
+		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(200));
+		return driver;
 	}
 	
 
